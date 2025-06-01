55aaae26d66e1d8792bb78e9c2b7b4aefea2df2b
Eduard Gabriel Ionescu <eduardg.ionescug@gmail.com>
2025-06-01 14:23:02 +0300

    Update vite.config.js

--- smart-link-frontend/vite.config.js
+++ smart-link-frontend/vite.config.js
@@ -6,10 +6,11 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
   tailwindcss()],
+
   server: {
     allowedHosts: [
-      'all',
-      '18fe-2a02-a58-9101-4f00-c9b7-433f-e691-865a.ngrok-free.app'
+      'TRUE',
+      '7b6f-2a02-a58-9101-4f00-39d2-45eb-9fd4-cbba.ngrok-free.app'
       // you can also add other hosts or 'all' to allow everything (less secure)
       // or use '*' to allow all (for dev only)
     ],
